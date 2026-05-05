5335b4fd0462bf9dfc2876f5b4599d73d8eb8ba1
José Edson Junior <edsonjunior9701@outlook.com>
2026-05-05 11:20:55 -0300

    teste do palindromo

---
+++ Palindromo/Palindromo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.Contracts;
+
+Console.Write("Digite uma palavra: ");
+string palindromo = Console.ReadLine().ToLower();
+
+if (IsPalindromo(palindromo))
+    Console.WriteLine("É palindromo!");
+else
+    Console.WriteLine("Não é palindromo.");
+
+bool IsPalindromo(string palindromo)
+{
+    string palindromoLimpo = "";
+
+    if (string.IsNullOrWhiteSpace(palindromo))
+        return false;
+    
+
+    foreach (char c in palindromo)// limpa texto sem o Trim()
+    {
+        if (c != ' ')
+            palindromoLimpo += c;
+    }
+
+    int inicio = 0;
+    int fim = palindromoLimpo.Length - 1;
+
+    while (inicio < fim) // verifica se texto sem espaço e toLower é palindromo
+    {
+        if (palindromoLimpo[inicio] != palindromoLimpo[fim])
+            return false;
+
+        inicio++;
+        fim--;
+    }
+
+    return true;
+}
